d3757d20ccf7d9cdff4e2dab25145347f53ba531
Sami Ullah <sami.ullah@invozone.com>
2022-08-27 19:25:59 +0500

    console.log() by default converts octal number to decimal

--- others/findAndReplaceNum.js
+++ others/findAndReplaceNum.js
@@ -19,3 +19,20 @@ const findAndReplaceNum = (number, toFind, toReplace) => {
 module.exports = {
     findAndReplaceNum,
 }
+
+
+const func = (number) => { 
+    console.log({ number })
+}
+
+func(12345) // logs => 12345
+func(012345) // logs => 5349
+
+/*
+func(012345) // logs => 5349 
+
+FYI: console.log() by default converts octal number to decimal
+
+octal numbers can start with leading zero.
+In given function, ocatal number is passed as a argument and logged as a decimal
+*/
