3b043675d898bdd63faf14b014523285e186d3bc
Sami Ullah <sami.ullah@invozone.com>
2022-09-05 03:12:02 +0500

    fix lengthOfLongestSubstring function for edge cases

--- leetcode/lengthOfLongestSubstring/index.js
+++ leetcode/lengthOfLongestSubstring/index.js
@@ -2,43 +2,44 @@
 
 const lengthOfLongestSubstring = (s) => {
 
-    if(s.length == 1) return 1;
+    if (s.length == 1) return 1;
+    if (new Set(s).size == s.length) return s.length;
 
     let longestStringLength = 0;
     let longestString = '';
     let lookUp = {};
     let substrStartIndex = 0;
+    let substrLastIndex = 0;
 
     for (let i = 0; i < s.length; i++) {
         const char = s[i];
-        if (!lookUp[char]) {
-            lookUp[char] = 1;
+        if (!lookUp[char] && lookUp[char] !== 0) {
+            lookUp[char] = i;
+            substrLastIndex = i+1;
 
         } else {
-            let tempString = s.slice(substrStartIndex, i);
-            if (tempString.length > longestStringLength) {
-                longestString = tempString;
-                longestStringLength = tempString.length;
+            if(lookUp[char] >= substrStartIndex) {
+                let tempString = s.slice(substrStartIndex, i);
+                if(tempString.length > longestString.length) longestString = tempString;
+                substrStartIndex = lookUp[char] + 1;
+                // substrLastIndex++
             }
-
-            // reset them.
-            lookUp = { [char]: 1 };
-            substrStartIndex = i;
+            lookUp[char] = i;
         }
 
+        console.log(`\n\n\n ${i}-Each==>`, {  longestStringLength, charOfInteration: char, lookUp, substrStartIndex, s, })
     }
-    const len = longestString.length;
-    return len;
+
+
+    tempString = s.slice(substrStartIndex, substrLastIndex);
+    if(tempString.length > longestString.length) longestString = tempString;
+
+    return longestString.length;
 }
 
 
-
-
-// let s = "pwwke";
-// let s = "pwpwpwk";
-// let s = 'bbbbb'
-// // let s = '  '
-// const result = lengthOfLongestSubstring(s);
-// console.log({ result });
+let s = 'aab'
+const result = lengthOfLongestSubstring(s);
+console.log({ result });
 
 module.exports = { lengthOfLongestSubstring };
--- leetcode/lengthOfLongestSubstring/index.test.js
+++ leetcode/lengthOfLongestSubstring/index.test.js
@@ -1,19 +1,50 @@
-const { lengthOfLongestSubstring } = require('./')
+const  {lengthOfLongestSubstring}  = require('./')
 // s = "abcabcbb"
 // Output: 3
 
-describe("Find the length of longest substring", () => {
+describe("Find the length of longest unique chracters substring", () => {
 
-   it("Cases", ()=> {
-    expect(lengthOfLongestSubstring("abcabcbb")).toBe(3);
-    expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
-    expect(lengthOfLongestSubstring("pwwkew")).toBe(3);
+    it("For repeated substring", () => {
+        expect(lengthOfLongestSubstring("abcabcbb")).toBe(3);
+        expect(lengthOfLongestSubstring("aaaabcabcbb")).toBe(3);
+        expect(lengthOfLongestSubstring("bbbbb")).toBe(1);
+        expect(lengthOfLongestSubstring("pwwkew")).toBe(3);
+    })
 
-    expect(lengthOfLongestSubstring("")).toBe(0);
-    expect(lengthOfLongestSubstring(" ")).toBe(1);
-    expect(lengthOfLongestSubstring("  ")).toBe(1);
 
-    expect(lengthOfLongestSubstring("a")).toBe(1);
-    expect(lengthOfLongestSubstring("aa")).toBe(1);
-   })
+    it("For empty spaces", () => {
+        expect(lengthOfLongestSubstring("")).toBe(0);
+        expect(lengthOfLongestSubstring(" ")).toBe(1);
+        expect(lengthOfLongestSubstring("  ")).toBe(1);
+    })
+
+
+    it("For single character or space", () => {
+        expect(lengthOfLongestSubstring("a")).toBe(1);
+        expect(lengthOfLongestSubstring(" ")).toBe(1);
+    })
+
+
+    it("For single repeated character", () => {
+        expect(lengthOfLongestSubstring("aa")).toBe(1);
+    })
+
+
+    it("For only unique characters", () => {
+        expect(lengthOfLongestSubstring("au")).toBe(2);
+        expect(lengthOfLongestSubstring("aug")).toBe(3);
+        expect(lengthOfLongestSubstring("augh")).toBe(4);
+    })
+
+
+    it("For unique characters at the head", () => {
+        expect(lengthOfLongestSubstring("aba")).toBe(2);
+        expect(lengthOfLongestSubstring("adfbab")).toBe(4);
+    })
+
+
+    it("For unique characters at the tail", () => {
+        expect(lengthOfLongestSubstring("aab")).toBe(2);
+    })
+
 })
